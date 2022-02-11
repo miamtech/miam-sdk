0fe55f2de8f4b41567ef9088f337976e044caca9
tibo <thibault.corbrion@miam.tech>
2022-02-11 15:34:38 +0100

    basket preview view

---
+++ androidSDK/src/main/java/com/miam/kmm_miam_sdk/android/ui/components/basketPreview/basketPreview.kt
@@ -0,0 +1,175 @@
+package com.miam.kmm_miam_sdk.android.ui.components.basketPreview
+
+import androidx.compose.foundation.*
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+
+import androidx.compose.material.icons.filled.Close
+
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+import androidx.compose.ui.graphics.Color
+
+import androidx.compose.ui.text.font.FontWeight
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+import com.miam.kmm_miam_sdk.component.basketPreview.BasketPreviewViewModel
+
+class BasketPreview(val recipeId :Int) {
+
+   private  val vmBasketPreview = BasketPreviewViewModel(recipeId)
+
+    @Composable
+    fun content() {
+
+        val state by vmBasketPreview.uiState.collectAsState()
+
+        Scaffold(
+            content = {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
+                        .verticalScroll(rememberScrollState())
+                ) {
+                    Row(
+                        Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    )
+                    {
+                        Text(
+                            text = "1 repas ajouté à votre panier :",
+                            style = MaterialTheme.typography.h5.copy(
+                                color = Color(0xff037E92),
+                                fontSize = 22.sp,
+                                fontWeight = FontWeight.Bold
+                            ),
+                            modifier = Modifier
+                                .wrapContentWidth(Alignment.CenterHorizontally)
+                                .padding(horizontal = 30.dp)
+                        )
+                        IconButton(
+                            modifier = Modifier
+                                .size(36.dp)
+                                .border(
+                                    border = BorderStroke(
+                                        width = 1.dp,
+                                        color = Color(0xffD9D9D9)
+                                    ),
+                                    shape = CircleShape
+                                ),
+                            onClick = {
+                                // TODO
+                            }) {
+                            Icon(
+                                tint = Color(0xff037E92),
+                                imageVector = Icons.Default.Close,
+                                contentDescription = "Drop-Down Arrow"
+                            )
+                        }
+                    }
+                    Spacer(modifier = Modifier.padding(vertical = 8.dp))
+                    if (state.lines.isEmpty()) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxHeight()
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            CircularProgressIndicator()
+                        }
+                    } else {
+                        if (recipeId != -1) {
+                            val recipeLine =
+                                state.lines.first { line -> line.id == recipeId && line.isRecipe }
+                            basketPreviewLine(line = recipeLine)
+
+                            if (recipeLine.entries?.found?.isNotEmpty() == true) {
+                                Column {
+                                    recipeLine.entries!!.found.forEach { entry ->
+                                        entriesLine(entry)
+                                    }
+                                }
+                            }
+                            if (recipeLine.entries?.notFound?.isNotEmpty() == true) {
+                                Spacer(modifier = Modifier.padding(vertical = 16.dp))
+                                expendableEntryLine(
+                                    recipeLine.entries!!.notFound,
+                                    backGroundColor = Color(0xffEDEDED),
+                                    fontColor = Color(0xff252525),
+                                    title = "Article(s) indisponible(s) (${recipeLine.entries!!.notFound.size})"
+                                )
+                            }
+                            if (recipeLine.entries?.oftenDeleted?.isNotEmpty() == true) {
+                                Spacer(modifier = Modifier.padding(vertical = 16.dp))
+                                expendableEntryLine(
+                                    recipeLine.entries!!.oftenDeleted,
+                                    backGroundColor = Color(0xffD9EFF2),
+                                    fontColor = Color(0xff007E92),
+                                    title = "Déjà dans vos placards ? (${recipeLine.entries!!.oftenDeleted.size})",
+                                    click = {}
+                                )
+                            }
+                            if (recipeLine.entries?.removed?.isNotEmpty() == true) {
+                                Spacer(modifier = Modifier.padding(vertical = 16.dp))
+                                expendableEntryLine(
+                                    recipeLine.entries!!.removed,
+                                    backGroundColor = Color(0xffBBBBBB),
+                                    fontColor = Color(0xff252525),
+                                    title = "Article(s) retiré(s) du panier (${recipeLine.entries!!.removed.size})",
+                                    click = {}
+                                )
+                            }
+                        } else {
+                            LazyColumn {
+                                items(state.lines) { line ->
+                                    basketPreviewLine(line)
+                                    Spacer(modifier = Modifier.padding(vertical = 8.dp))
+                                }
+                            }
+
+                        }
+
+                    }
+                    Spacer(modifier = Modifier.padding(vertical = 32.dp))
+                }
+              },
+            bottomBar = { BottomAppBar(backgroundColor = Color.White) {  Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = Color.White),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+            ) {
+                Button(
+                    border = BorderStroke(1.dp, Color(0xff007E92)),
+                    shape = RoundedCornerShape(50), 
+                    colors = ButtonDefaults.outlinedButtonColors(contentColor = Color(0xff007E92)),
+                    onClick = { /*TODO*/ }) {
+                    Text(text = "Retirer le repas")
+                }
+
+                Button(
+                    shape = RoundedCornerShape(50),
+                    colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.White, backgroundColor = Color(0xff007E92)),
+                    onClick = { /*TODO*/ }) {
+                    Text(text = "Continuer mes courses")
+                }
+            }
+            }
+            }
+        )
+    }
+}
---
+++ androidSDK/src/main/java/com/miam/kmm_miam_sdk/android/ui/components/basketPreview/entryLine.kt
@@ -0,0 +1,122 @@
+package com.miam.kmm_miam_sdk.android.ui.components.basketPreview
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberImagePainter
+import com.miam.kmm_miam_sdk.android.ui.components.Counter
+import com.miam.kmm_miam_sdk.android.ui.components.CounterModifier
+import com.miam.kmm_miam_sdk.android.ui.components.Price
+import com.miam.kmm_miam_sdk.android.ui.components.common.clickable
+import com.miam.kmm_miam_sdk.miam_core.model.BasketEntry
+
+@Composable
+fun entriesLine(entry: BasketEntry) {
+    val price = Price(price = 14.00, isTotalPrice = true)
+
+    Spacer(modifier = Modifier.padding(vertical = 4.dp))
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        IconButton(
+            modifier = Modifier
+                .size(30.dp)
+                .border(
+                    border = BorderStroke(
+                        width = 1.dp,
+                        color = Color(0xffD9D9D9)
+                    ),
+                    shape = RoundedCornerShape(100.dp)
+                ),
+            onClick = {
+                //TODO
+            }) {
+            Icon(
+                tint = Color.Gray,
+                imageVector = Icons.Default.Delete,
+                contentDescription = "Drop-Down Arrow"
+            )
+        }
+        Image(
+            painter = rememberImagePainter("https://www.presse-citron.net/app/uploads/2019/01/oeuf-instagram.jpg"),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .height(72.dp)
+                .width(72.dp)
+                .clip(RoundedCornerShape(8.dp)),
+        )
+
+        Column() {
+            Text(
+                text = "Oeuf",
+                style = MaterialTheme.typography.h5.copy(
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                ),
+            )
+            Text(
+                text = entry.id.toString(),
+                color= Color(0xff9F9F9F),
+                style = MaterialTheme.typography.h5.copy(
+                    fontSize = 14.sp,
+                ),
+            )
+            Spacer(modifier = Modifier.padding(vertical = 4.dp))
+            clickable(
+                onClick = { /*TODO*/ },
+                children = {
+                    Text(
+                        text ="Changer d'article",
+                        color = Color(0xff037E92),
+                        overflow = TextOverflow.Ellipsis,
+                        style = MaterialTheme.typography.h5.copy(
+                            fontSize = 14.sp,
+                            textDecoration = TextDecoration.Underline
+                        ),
+
+                        )
+                }
+            )
+            Spacer(modifier = Modifier.padding(vertical = 4.dp))
+
+            Surface(
+                border = BorderStroke(1.dp, Color(0xff676767)),
+                contentColor = Color(0xff676767),
+                shape = CircleShape
+            ) {
+                Text(modifier = Modifier.padding(8.dp),text = "pour 14 recettes")
+            }
+
+            Row (verticalAlignment = Alignment.CenterVertically) {
+                price.content()
+                Spacer(modifier = Modifier.padding(horizontal = 4.dp))
+                Counter(
+                    count = 1,
+                    increase = { /*TODO*/ },
+                    decrease = { /*TODO*/ },
+                    counterModifier = CounterModifier(
+                        iconModifier = Modifier.width(0.dp))
+                )
+            }
+        }
+    }
+}
