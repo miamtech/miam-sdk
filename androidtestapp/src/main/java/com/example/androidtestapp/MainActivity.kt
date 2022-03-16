761881ef004a2822b4ec9184f5fbf5d0d6264b41
tibo <thibault.corbrion@miam.tech>
2022-03-16 11:54:23 +0100

    update demo

--- androidtestapp/src/main/java/com/example/androidtestapp/MainActivity.kt
+++ androidtestapp/src/main/java/com/example/androidtestapp/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.androidtestapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.miam.kmm_miam_sdk.android.di.KoinInitilizer
 import com.miam.kmm_miam_sdk.android.ui.components.recipeCard.RecipeView
 
@@ -15,11 +31,9 @@ import com.miam.kmm_miam_sdk.handler.PointOfSaleHandler
 import com.miam.kmm_miam_sdk.handler.UserHandler
 
 import com.miam.kmm_miam_sdk.miam_core.model.RetailerProduct
+import com.miam.kmm_miam_sdk.miam_core.model.SuggestionsCriteria
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.last
+import kotlinx.coroutines.flow.*
 import org.koin.core.component.KoinComponent
 
 import kotlin.random.Random
@@ -28,48 +42,98 @@ import kotlin.random.Random
 @ExperimentalMaterialApi
 @ExperimentalComposeUiApi
 @OptIn(InternalCoroutinesApi::class)
-class MainActivity : AppCompatActivity(), KoinComponent,  CoroutineScope by CoroutineScope(
+class MainActivity : ComponentActivity(), KoinComponent,  CoroutineScope by CoroutineScope(
     Dispatchers.Main) {
 
 
-    private val retailerBasketSubject : MutableSharedFlow<MutableList<CoursesUProduct>> = MutableSharedFlow()
-    private val customerSubject : MutableSharedFlow< MutableList<CoursesUCustomer>> = MutableSharedFlow()
-    private val PointOfSaleSubject : MutableSharedFlow< MutableList<CoursesUPointOfSale>> = MutableSharedFlow()
+    private val retailerBasketSubject : MutableStateFlow<ExampleState> = MutableStateFlow(ExampleState())
     private val basketHandler = BasketHandler()
-    private var list : MutableList<CoursesUProduct> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         KoinInitilizer.init(this)
         setListenToRetailerBasket(basketHandler)
+        setPushProductToBasket(basketHandler)
         PointOfSaleHandler.updateStoreId("35290")
         PointOfSaleHandler.setSupplier(7)
         UserHandler.updateUserId("ed0a471a4bdc755664db84068119144b3a1772d8a6911057a0d6be6a3e075120")
         initFakeBasket()
-        setContentView(R.layout.activity_main)
-        val recipeView = findViewById(R.id.miam2) as RecipeView
-        recipeView.idRecipe = 9075
-        val recipeView2 = findViewById(R.id.miam3) as RecipeView
-        recipeView2.idRecipe = 1
-        val buttonAdd = findViewById(R.id.Add) as Button
-        buttonAdd.setOnClickListener {
-            pushProduct()
+        setContent {
+            content(this.applicationContext,retailerBasketSubject)
         }
-        val buttonRemove = findViewById(R.id.Remove) as Button
-        buttonRemove.setOnClickListener {
-            removeProduct()
+    }
+
+    @Composable
+    fun content(context: Context, retailerBasketSubject : MutableStateFlow<ExampleState>){
+
+        val state = retailerBasketSubject.asStateFlow().collectAsState()
+
+        val recipe1 =  RecipeView(context)
+        val recipe2 =  RecipeView(context)
+
+        recipe1.bind(recipeId = 305)
+        recipe2.bind(criteria = RandomCriteria())
+
+        Column(
+            Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Panier du client", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            Text(text = "Total Panier -> ${state.value.items.sumOf { it.price }} €")
+            Divider()
+            Row(Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+                Button(onClick = { pushProduct() }) {
+                    Text(text = "Ajouter")
+                }
+                Button(onClick = { removeProduct() }) {
+                    Text(text = "Retirer")
+                }
+            }
+            Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                state.value.items.forEach{
+                    Box(modifier = Modifier.padding(4.dp).clip(RoundedCornerShape(16.dp))) {
+                        Box(
+                            Modifier
+                                .background(Color.Gray).padding(vertical =  4.dp , horizontal = 8.dp) ,
+                        ) {
+                            Column() {
+                                Text(text = it.name, color = Color.White, fontWeight = FontWeight.Bold)
+                                Text(text = " X ${it.quantity} ", color = Color.White)
+                            }
+                        }
+                    }
+                }
+            }
+            Divider()
+            recipe1.Content()
+            recipe2.Content()
         }
+    }
+
+    private fun RandomCriteria() :SuggestionsCriteria{
+        val randomProductId = mutableListOf<String>()
+        for (i in 0..2) {
+            val randomElement = productSampleCoursesU.random()
+            randomProductId.add(randomElement.id)
+        }
+
+       return SuggestionsCriteria(shelfIngredientsIds = randomProductId )
     }
 
     private fun initFakeBasket(){
         val initialBasket = mutableListOf<CoursesUProduct>()
         for (i in 0..3) {
             val randomElement = productSampleCoursesU.random()
-            initialBasket.add(randomElement.copy(quantity = Random.nextInt(1,4)))
+            initialBasket.add(randomElement.copy(quantity = Random.nextInt(1, 4)))
         }
+
+        retailerBasketSubject.value.items.addAll(initialBasket)
         launch {
-            list= initialBasket
-            retailerBasketSubject.emit(initialBasket)
+            retailerBasketSubject.emit(retailerBasketSubject.value)
         }
     }
 
@@ -81,7 +145,7 @@ class MainActivity : AppCompatActivity(), KoinComponent,  CoroutineScope by Coro
         launch {
             retailerBasketSubject.collect {
                 print("DEMO basket EMT")
-                callback(it.map { product -> coursesUProductTORetailerProduct(product)  })
+                callback(it.items.map { product -> coursesUProductTORetailerProduct(product)  })
             }
         }
     }
@@ -93,15 +157,40 @@ class MainActivity : AppCompatActivity(), KoinComponent,  CoroutineScope by Coro
     private fun pushProduct(){
         launch {
             val randomElement = productSampleCoursesU.random()
-            list.add(randomElement.copy(quantity = Random.nextInt(1,4)))
-            retailerBasketSubject.emit(list)
+            retailerBasketSubject.value.items.add(randomElement.copy(quantity = Random.nextInt(1,4)))
+            retailerBasketSubject.emit(ExampleState(retailerBasketSubject.value.items))
         }
     }
 
     private fun removeProduct(){
         launch {
-            list.removeAt(Random.nextInt(list.size))
-            retailerBasketSubject.emit(list)
+            if(retailerBasketSubject.value.items.isNotEmpty()) {
+                retailerBasketSubject.value.items.removeAt(Random.nextInt(retailerBasketSubject.value.items.size))
+                retailerBasketSubject.emit(ExampleState(retailerBasketSubject.value.items))
+            }
+        }
+    }
+
+    private fun setPushProductToBasket(basketHandler: BasketHandler) {
+        basketHandler.pushProductsToBasket = ::pushProductToRetailer
+
+    }
+
+    private fun pushProductToRetailer(coursesUProducts: List<RetailerProduct>){
+
+        coursesUProducts.forEach { rp ->
+          val productToUpdateIdx =  retailerBasketSubject.value.items.indexOfFirst { it.id == rp.id }
+            if(productToUpdateIdx == -1){
+                retailerBasketSubject.value.items.add(CoursesUProduct(rp.id, rp.id, rp.quantity, 0.0))
+            } else if( rp.quantity == 0) {
+                retailerBasketSubject.value.items.removeAt(productToUpdateIdx)
+            } else {
+                retailerBasketSubject.value.items[productToUpdateIdx] = retailerBasketSubject.value.items[productToUpdateIdx].copy(quantity = rp.quantity)
+            }
+
+        }
+        launch {
+            retailerBasketSubject.emit(ExampleState(retailerBasketSubject.value.items))
         }
     }
 
@@ -124,9 +213,12 @@ class MainActivity : AppCompatActivity(), KoinComponent,  CoroutineScope by Coro
             CoursesUProduct("6134471","Banane Cavendish BIO, calibre P14, catégorie 2, Republique Dominicaine, ruban 5 fruits",1,1.99),
             CoursesUProduct("6182231","Beurre de cacahuète creamy MENGUY'S 454g",1,3.99),
             CoursesUProduct("352902000000909790","Baguette Triskel",1,1.0),
+            CoursesUProduct("88455","Emmental français rapé U, 29%mg, 100g",1,0.84),
+            CoursesUProduct("2107653","Lait de coco KARA, 200ml",1,0.94),
+            CoursesUProduct("5319173","Filet de blanc de poulet U, France, barquette",1,6.06),
+            CoursesUProduct("6511680","Curry tradition en poudre DUCROS, 53g",1,3.40),
         )
     }
-
 }
 
 data class CoursesUProduct(
@@ -142,4 +234,8 @@ data class CoursesUCustomer(
 
 data class CoursesUPointOfSale(
     val identifier: String
+)
+
+class ExampleState(
+     val items: MutableList<CoursesUProduct> = mutableListOf()
 )
