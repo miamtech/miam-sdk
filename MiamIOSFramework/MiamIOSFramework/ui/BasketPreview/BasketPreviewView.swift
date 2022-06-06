949476d04303263f7a306c287fed96f8da988b22
Vincent Kergonna <kergonna@k-interactive.fr>
2022-06-06 17:09:06 +0200

    Extract bottom view

--- MiamIOSFramework/MiamIOSFramework/ui/BasketPreview/BasketPreviewView.swift
+++ MiamIOSFramework/MiamIOSFramework/ui/BasketPreview/BasketPreviewView.swift
@@ -149,23 +149,7 @@ public struct BasketPreviewView: View {
                     .padding(.bottom, Dimension.sharedInstance.lPadding)
             }
             
-            //Bottom View
-            HStack {
-                ZStack(alignment: .center) {
-                    Text("Retirer du panier")
-                        .foregroundColor(MiamColor.sharedInstance.bodyText)
-                        .font(.system(size: 16.0, weight: .regular, design: .default))
-                }.padding(.horizontal, Dimension.sharedInstance.mPadding).frame(minWidth: 155.0)
-                
-                ZStack(alignment: .center) {
-                    Text("Continuer mes courses")
-                        .foregroundColor(MiamColor.sharedInstance.white
-                        ).padding(.horizontal, Dimension.sharedInstance.sPadding)
-                        .font(.system(size: 16.0, weight: .bold, design: .default))
-                }.frame(maxWidth: .infinity)
-                    .frame(height: 64.0)
-                    .background(MiamColor.sharedInstance.primaryText)
-            }.frame(maxWidth: .infinity, alignment: .bottom).background(MiamColor.sharedInstance.white)
+            BasketPreviewBottomView()
         }
     }
 }
