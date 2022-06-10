9a2334f72557fde19737419b05fddd8a49b16eb4
Vincent Kergonna <kergonna@k-interactive.fr>
2022-06-10 14:22:55 +0200

    Add IngredientNotAddedRow view

---
+++ MiamIOSFramework/MiamIOSFramework/ui/BasketPreview/IngredientNotInBasketRow.swift
@@ -0,0 +1,44 @@
+//
+//  IngredientNotAddedRow.swift
+//  MiamIOSFramework
+//
+//  Created by Vincent Kergonna on 10/06/2022.
+//
+
+import Foundation
+import SwiftUI
+
+struct IngredientNotInBasketRow: View {
+    let name: String
+    let addIngredientAction: () -> Void
+    private let addIngredientText = "Ajouter"
+    private let addIngredientIcon = "PlusGreen"
+    var body: some View {
+        HStack {
+            Text(name)
+                .font(.system(size: 16.0, weight: .bold, design: .default))
+                .foregroundColor(MiamColor.sharedInstance.bodyText)
+
+            Spacer()
+
+            Button(action: addIngredientAction) {
+                Image(addIngredientIcon)
+                    .resizable()
+                    .aspectRatio( contentMode: .fit)
+                    .frame(width: 30, height: 30, alignment: .center)
+                    .foregroundColor(MiamColor.sharedInstance.primary)
+                Text(addIngredientText)
+                    .font(.system(size: 16.0, weight: .bold, design: .default))
+                    .foregroundColor(MiamColor.sharedInstance.primary)
+            }
+
+        }.padding(.horizontal, Dimension.sharedInstance.lPadding)
+            .padding(.bottom, Dimension.sharedInstance.lPadding)
+    }
+}
+
+struct IngredientNotInBasketRow_Previews: PreviewProvider {
+    static var previews: some View {
+        IngredientNotInBasketRow(name: "Oeufs", addIngredientAction: {})
+    }
+}
