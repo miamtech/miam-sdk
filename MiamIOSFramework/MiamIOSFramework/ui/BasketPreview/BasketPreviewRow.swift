a57fa67a89f9733cbb504aa43e755e88d96c1339
Vincent Kergonna <kergonna@k-interactive.fr>
2022-06-10 16:54:06 +0200

    Plug BasketPreviewRow on view model

--- MiamIOSFramework/MiamIOSFramework/ui/BasketPreview/BasketPreviewRow.swift
+++ MiamIOSFramework/MiamIOSFramework/ui/BasketPreview/BasketPreviewRow.swift
@@ -11,35 +11,40 @@ struct BasketPreviewRow: View {
     @State var stepperValue: Float = 0.0
 
     private let productImageDimensions = CGSize(width: 90, height: 90)
+    let productName: String
+    let productBrandName: String
+    let productDescription: String
+    let productPrice: String
 
+    let removeProductAction: () -> Void
+    let replaceProductAction: () -> Void
+
+
+    private let moreInformationButtonTitle = "Plus d'infos"
+    private let replaceIngredientButtonTitle = "Remplacer"
     var body: some View {
         VStack(alignment: .leading) {
             HStack(alignment: .top) {
                 AsyncImage(url: URL(string: "https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/410.svg")!,
-                           placeholder: { ProgressView() }, height: 100.0).frame(width: productImageDimensions.width, height: productImageDimensions.height, alignment: .topLeading)
+                           placeholder: { ProgressView() }, height: productImageDimensions.height).frame(width: productImageDimensions.width, height: productImageDimensions.height, alignment: .topLeading)
                 
                 VStack (alignment: .leading) {
                     
-                    Text("Bière blonde (40 cl)")
+                    Text(productName)
                         .foregroundColor(MiamColor.sharedInstance.black)
                         .font(.system(size: 16, weight: .bold, design: .default))
                         .padding(.leading, Dimension.sharedInstance.sPadding)
                     
-                    Text("3 MONTS")
+                    Text(productBrandName)
                         .foregroundColor(MiamColor.sharedInstance.bodyText)
                         .font(.system(size: 13, weight: .medium, design: .default))
                         .padding(.leading, Dimension.sharedInstance.sPadding)
                         .padding(.top, Dimension.sharedInstance.borderWidth)
                     
-                    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus. 0.15 kg")
+                    Text(productDescription)
                         .foregroundColor(MiamColor.sharedInstance.bodyText)
                         .font(.system(size: 16, weight: .light, design: .default))
                         .padding(.leading, Dimension.sharedInstance.sPadding)
-                        .padding(.top, Dimension.sharedInstance.borderWidth)
-                    
-                    Text("Plus d’infos").foregroundColor(MiamColor.sharedInstance.black).font(.system(size: 13, weight: .medium, design: .default))
-                        .padding(.leading, Dimension.sharedInstance.sPadding)
-                        .padding(.top, Dimension.sharedInstance.borderWidth)
                         .padding(.top, Dimension.sharedInstance.borderWidth)
                 }
                 
@@ -53,7 +58,7 @@ struct BasketPreviewRow: View {
             
             //Price
             VStack {
-                Text("1,59 €")
+                Text("\(productPrice)€")
                     .foregroundColor(MiamColor.sharedInstance.primaryText)
                     .font(.system(size: 20, weight: .heavy, design: .default))
             }.frame(maxWidth: .infinity, alignment: .trailing)
@@ -68,8 +73,8 @@ struct BasketPreviewRow: View {
                         Image("sync").resizable()
                             .renderingMode(.original)
                             .frame(width: 30, height: 30, alignment: .topTrailing)
-                        Text("Remplacer")
-                            .foregroundColor(MiamColor.sharedInstance.primaryText).font(.system(size: 20, weight: .heavy, design: .default))
+                        Text(replaceIngredientButtonTitle)
+                            .foregroundColor(MiamColor.sharedInstance.primaryText).font(.system(size: 20, weight: .semibold, design: .default))
                     }.frame(width: 145, height: 30, alignment: .topTrailing)
                     
                     Spacer()
@@ -83,7 +88,6 @@ struct BasketPreviewRow: View {
                 }
             }
             
-            //Divider
             Divider()
                 .background(MiamColor.sharedInstance.borderBottom)
                 .padding(.top, Dimension.sharedInstance.mPadding)
@@ -95,6 +99,6 @@ struct BasketPreviewRow: View {
 
 struct ProductRow_Previews: PreviewProvider {
     static var previews: some View {
-        BasketPreviewRow()
+        BasketPreviewRow(productName: "Bière blonde (40 cl)", productBrandName: "3 MONTS", productDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus. 0.15 kg", productPrice: "1.59", removeProductAction: {}, replaceProductAction: {})
     }
 }
--- MiamIOSFramework/MiamIOSFramework/ui/BasketPreview/BasketPreviewView.swift
+++ MiamIOSFramework/MiamIOSFramework/ui/BasketPreview/BasketPreviewView.swift
@@ -26,6 +26,14 @@ public struct BasketPreviewView: View {
         viewModel.setEvent(event: BasketPreviewContractEvent.AddEntry(entry: entry))
     }
 
+    func removeProduct(_ entry: BasketEntry) {
+        viewModel.setEvent(event: BasketPreviewContractEvent.RemoveEntry(entry: entry))
+    }
+
+    func replaceProduct(_ entry: BasketEntry) {
+
+    }
+
     public var body: some View {
         VStack {
             // Top Bar
@@ -48,7 +56,14 @@ public struct BasketPreviewView: View {
                 //List
                 VStack {
                     ForEach(viewModel.productsInBasket, id: \.self) { entry in
-                        BasketPreviewRow()
+                        let previewLine = BasketPreviewLine.fromBasketEntry(entry: entry)
+
+                        BasketPreviewRow(productName: previewLine.title, productBrandName: previewLine.productBrand,
+                                         productDescription: previewLine.productDescription, productPrice: previewLine.price, removeProductAction: {
+                            removeProduct(entry)
+                        }, replaceProductAction: {
+
+                        })
                     }
                 }
 
@@ -135,5 +150,50 @@ public struct BasketPreviewView: View {
 
             }
         }
+    }
+}
+
+internal extension BasketPreviewLine {
+    static func fromBasketEntry(entry: BasketEntry) -> BasketPreviewLine   {
+        let item = entry.selectedItem
+        let beI = entry.attributes!.basketEntriesItems?.first(where: { item in
+            item.itemId == entry.attributes?.selectedItemId
+        })
+        var price = 0.0
+        if let beItem = beI, let quantity = entry.attributes?.quantity, let itemPrice = beItem.unitPrice?.doubleValue {
+            price = itemPrice * quantity.doubleValue
+        }
+        let gEntry = entry.relationships?.groceriesEntry?.data
+        let recipesCount =  gEntry?.attributes?.recipeIds?.count ?? 1
+        let title = entry.relationships?.groceriesEntry?.data.attributes?.name ?? ""
+        var description = ""
+        if let name = item?.attributes?.name, let capacityUnit =  item?.attributes?.capacityUnit {
+            description = "\(name) \n (\(capacityUnit)"
+        }
+        var brand = ""
+        if let productBrand = item?.attributes?.brand {
+            brand = productBrand
+        }
+        return BasketPreviewLine(
+            id: entry.id,
+            record: entry,
+            isRecipe: false,
+            inlineTag: recipesCount > 1 ? "Pour \(recipesCount) recettes" : nil,
+            title: title,
+            picture: item?.attributes?.image ?? "",
+            bplDescription: [description, brand],
+            price: "\(price)",
+            count: entry.attributes?.quantity?.int32Value ?? 1,
+            entries: nil,
+            _displayMode: false
+        )
+    }
+
+    var productBrand: String {
+        return bplDescription.count > 1 ? bplDescription[1] : ""
+    }
+
+    var productDescription: String {
+        return bplDescription.count > 0 ? bplDescription[0] : ""
     }
 }
