feabec6bc3834ae52b9d631847a90cadca59ccfd
Vincent Kergonna <kergonna@k-interactive.fr>
2022-06-30 17:00:08 +0200

    Display recipes in a lazy vstack

--- MiamIOSFramework/MiamIOSFramework/ui/Catalog/CatalogRecipePageSuccessView.swift
+++ MiamIOSFramework/MiamIOSFramework/ui/Catalog/CatalogRecipePageSuccessView.swift
@@ -9,14 +9,21 @@ import Foundation
 import SwiftUI
 
 internal struct CatalogRecipesPageSuccessView: View {
-    let recipesListPageModel: RecipeListPageVM
+    @ObservedObject var recipesListPageModel: RecipeListPageVM
+
+    init(viewModel: RecipeListPageVM) {
+        recipesListPageModel = viewModel
+        recipesListPageModel.loadPage()
+    }
+
     var body: some View {
         ScrollView {
-            VStack {
+            LazyVStack(alignment: .leading) {
+                Text(recipesListPageModel.title).font(Font.system(size: 20.0)).fontWeight(.heavy)
                 ForEach(recipesListPageModel.recipes, id: \.self) { recipe in
                     RecipeCardView(recipeId: recipe.id)
                 }
-            }
+            }.padding([.leading, .trailing], Dimension.sharedInstance.mlPadding)
         }
     }
 }
