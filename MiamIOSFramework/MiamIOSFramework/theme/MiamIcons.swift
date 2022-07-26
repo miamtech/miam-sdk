9d396d155078b1b63408cddd1c4e763422173cb8
Vincent Kergonna <kergonna@k-interactive.fr>
2022-07-26 14:49:11 +0200

    Add Image extension
    Create images from the main bundle or from miam SDK bundle

---
+++ MiamIOSFramework/MiamIOSFramework/theme/MiamIcons.swift
@@ -0,0 +1,13 @@
+//
+//  MiamIcons.swift
+//  MiamIOSFramework
+//
+//  Created by Vincent Kergonna on 26/07/2022.
+//
+
+import Foundation
+
+public enum MiamIcon: String {
+    case arrow = "miamArrow"
+    case back = "miamBack"
+}
