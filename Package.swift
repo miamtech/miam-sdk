17d77ec4c1e5cc28aec4669aef4d5a364eac1c98
Vincent Kergonna <kergonna@k-interactive.fr>
2022-07-29 11:00:50 +0200

    Fix swift package manifest

--- Package.swift
+++ Package.swift
@@ -7,19 +7,16 @@ let package = Package(
         .iOS(.v14),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MiamIOSFramework",
             targets: ["MiamIOSFramework", "miamCore"])
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MiamIOSFramework",
+            dependencies: ["miamCore"],
             path: "MiamIOSFramework/MiamIOSFramework",
             resources: [
                 .process("miam.xcassets"),
@@ -28,10 +25,11 @@ let package = Package(
         .binaryTarget(
             name: "miamCore",
             path: "frameworks/miamCore.xcframework"
-        )
+        ),
         .testTarget(
             name: "MiamIOSFrameworkTests",
-            path: "MiamIOSFramework/MiamIOSFrameworkTests",
-            dependencies: ["MiamIOSFramework"]),
+            dependencies: ["MiamIOSFramework"],
+            path: "MiamIOSFramework/MiamIOSFrameworkTests"
+        )
     ]
 )
