45be53168a0e7f0f7106dbc7d919c49a60c9f3f3
tibo <thibault.corbrion@miam.tech>
2022-08-02 16:54:53 +0200

    publish on maven + ci

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,15 @@
+
+
+plugins {
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+    `kotlin-dsl`
+}
+
 buildscript {
-
-
     repositories {
         gradlePluginPortal()
         google()
         mavenCentral()
-
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
@@ -25,3 +29,13 @@ tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
 
+extra.apply{
+    set("PUBLISH_GROUP_ID" , "tech.miam.sdk")
+    set("PUBLISH_VERSION" ,"2.1.0")
+    set("PUBLISH_ARTIFACT_ID", "miam-android-sdk")
+}
+
+/**
+ * Maven central deploy script
+ * */
+apply("${rootDir}/scripts/publish-root.gradle")
