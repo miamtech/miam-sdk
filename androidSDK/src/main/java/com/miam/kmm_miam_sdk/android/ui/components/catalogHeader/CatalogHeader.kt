051e1b58dc8f08eb8278861d3c9369d7cdb6aca5
tibo <thibault.corbrion@miam.tech>
2022-10-10 15:24:45 +0200

    stand alone header android

--- androidSDK/src/main/java/com/miam/kmm_miam_sdk/android/ui/components/catalogHeader/CatalogHeader.kt
+++ androidSDK/src/main/java/com/miam/kmm_miam_sdk/android/ui/components/catalogHeader/CatalogHeader.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.miam.kmmMiamCore.component.catalogFilter.CatalogFilterViewModel
+import com.miam.kmm_miam_sdk.android.ressource.Image
 import com.miam.kmm_miam_sdk.android.theme.Colors.primary
 import com.miam.kmm_miam_sdk.android.theme.Colors.white
 import com.miam.kmm_miam_sdk.android.theme.Template
@@ -31,11 +32,10 @@ import com.miam.kmm_miam_sdk.android.theme.Typography.subtitleBold
 import com.miam.kmm_miam_sdk.android.ui.components.catalog.CatalogFilter
 import com.miam.kmm_miam_sdk.android.ui.components.catalog.CatalogSearch
 import com.miam.kmm_miam_sdk.android.ui.components.catalog.customization.CatalogColor.headerTextColor
-import com.miam.kmm_miam_sdk.android.ui.components.catalog.customization.CatalogImage.back
-import com.miam.kmm_miam_sdk.android.ui.components.catalog.customization.CatalogImage.favorite
+import com.miam.kmm_miam_sdk.android.ui.components.catalog.customization.CatalogImage
 import com.miam.kmm_miam_sdk.android.ui.components.catalog.customization.CatalogImage.recipeId
 import com.miam.kmm_miam_sdk.android.ui.components.catalog.customization.CatalogImage.trait
-import com.miam.kmm_miam_sdk.android.ui.components.catalog.customization.CatalogText.favoriteButtonText
+import com.miam.kmm_miam_sdk.android.ui.components.catalog.customization.CatalogText
 import com.miam.kmm_miam_sdk.android.ui.components.catalog.customization.CatalogText.headerTitle
 import com.miam.kmm_miam_sdk.android.ui.components.common.Clickable
 
@@ -62,7 +62,9 @@ fun CatalogHeader(showFullHeader: Boolean, isFavorit: Boolean, goToRecipeList: (
         searchOpened = false
     }
 
+    // TODO Refact with filter service
     val filter = CatalogFilter(catalogFilterVm, ::closeModal, goToRecipeList)
+    // TODO Refact with filter service
     val search = CatalogSearch(catalogFilterVm, ::closeModal, goToRecipeList)
 
     fun getActiveFilterCount(): Int {
@@ -70,181 +72,206 @@ fun CatalogHeader(showFullHeader: Boolean, isFavorit: Boolean, goToRecipeList: (
         return -1
     }
 
-    if (Template.CatalogHeader != null) {
-        Template.CatalogHeader?.let {
-            it(::openFilter, ::openSearch, goToFavorite, goToBack, ::getActiveFilterCount)
+    @Composable
+    fun HeaderTitle() {
+        Box(
+            Modifier.fillMaxWidth()
+        ) {
+            Row(
+                Modifier.padding(16.dp)
+            ) {
+                Image(
+                    painter = painterResource(recipeId),
+                    contentDescription = null,
+                    colorFilter = ColorFilter.tint(headerTextColor),
+                    modifier = Modifier.padding(end = 8.dp)
+                )
+                Box {
+                    Text(
+                        text = headerTitle,
+                        color = headerTextColor,
+                        style = subtitleBold,
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                    Image(
+                        painter = painterResource(trait),
+                        contentDescription = null,
+                        Modifier
+                            .align(Alignment.BottomEnd)
+                            .offset(y = 4.dp)
+                    )
+
+                }
+            }
         }
-    } else {
-        Column(Modifier.background(color = primary)) {
-            if (showFullHeader) {
+    }
+
+    @Composable
+    fun BackButton() {
+        Clickable(onClick = { goToBack() }) {
+            Image(
+                painter = painterResource(CatalogImage.back),
+                contentDescription = null,
+                colorFilter = ColorFilter.tint(white),
+                modifier = Modifier
+                    .rotate(180f)
+                    .padding(vertical = 8.dp)
+            )
+        }
+    }
+
+    @Composable
+    fun SearchButton() {
+        Clickable(onClick = { openSearch() }) {
+            Surface(
+                shape = CircleShape,
+                elevation = 8.dp,
+                modifier = Modifier.padding(horizontal = 10.dp)
+            ) {
                 Box(
                     Modifier
-                        .fillMaxWidth()
+                        .background(white)
+                        .padding(8.dp)
+                )
+                {
+                    Image(
+                        painter = painterResource(Image.search),
+                        contentDescription = null,
+                        colorFilter = ColorFilter.tint(primary),
+                    )
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun FilterButton() {
+        Clickable(onClick = { openFilter() }) {
+            Box {
+                Surface(
+                    shape = CircleShape,
+                    elevation = 8.dp,
+                    modifier = Modifier.padding(horizontal = 10.dp)
                 ) {
                     Row(
-                        Modifier.padding(16.dp)
+                        Modifier
+                            .background(white)
+                            .padding(8.dp)
+                    )
+                    {
+                        Image(
+                            painter = painterResource(Image.filter),
+                            contentDescription = null,
+                            colorFilter = ColorFilter.tint(primary),
+                        )
+
+                    }
+                }
+                if (getActiveFilterCount() != 0) {
+                    Box(
+                        modifier = Modifier
+                            .size(20.dp)
+                            .clip(CircleShape)
+                            .background(Color.Red)
+                            .align(Alignment.TopEnd)
+                    ) {
+                        Text(
+                            text = getActiveFilterCount().toString(),
+                            color = white,
+                            modifier = Modifier.align(Alignment.Center)
+
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun FavoriteButton() {
+        if (!isFavorit) {
+            if (showFullHeader) {
+                Box(modifier = Modifier
+                    .padding(horizontal = 10.dp)
+                    .border(
+                        border = BorderStroke(1.dp, white),
+                        shape = RoundedCornerShape(50)
+                    )
+                    .clickable { goToFavorite() }
+                ) {
+                    Row(
+                        Modifier
+                            .padding(horizontal = 16.dp, vertical = 8.dp)
+                            .background(primary),
+                        verticalAlignment = Alignment.CenterVertically
                     ) {
                         Image(
-                            painter = painterResource(recipeId),
+                            painter = painterResource(Image.favorite),
                             contentDescription = null,
-                            colorFilter = ColorFilter.tint(headerTextColor),
+                            colorFilter = ColorFilter.tint(white),
                             modifier = Modifier.padding(end = 8.dp)
                         )
-                        Box {
-                            Text(
-                                text = headerTitle,
-                                color = headerTextColor,
-                                style = subtitleBold,
-                                modifier = Modifier.align(Alignment.Center)
-                            )
+                        Text(
+                            text = CatalogText.favoriteButtonText,
+                            color = white
+                        )
+                    }
+                }
+            } else {
+                Clickable(onClick = { goToFavorite() } ) {
+                    Surface(
+                        shape = CircleShape,
+                        elevation = 8.dp,
+                        modifier = Modifier.padding(horizontal = 10.dp)
+                    ) {
+                        Box(
+                            Modifier
+                                .background(white)
+                                .padding(8.dp)
+                        )
+                        {
                             Image(
-                                painter = painterResource(trait),
+                                painter = painterResource(Image.favorite),
                                 contentDescription = null,
-                                Modifier
-                                    .align(Alignment.BottomEnd)
-                                    .offset(y = 4.dp)
+                                colorFilter = ColorFilter.tint(primary),
                             )
-
                         }
                     }
                 }
             }
-            Row(
-                Modifier
-                    .fillMaxWidth()
-                    .padding(12.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = if (showFullHeader) Arrangement.Start else Arrangement.SpaceBetween
-            ) {
-                if (!showFullHeader) {
-                    Clickable(onClick = { goToBack() }, children = {
-                        Image(
-                            painter = painterResource(back),
-                            contentDescription = null,
-                            colorFilter = ColorFilter.tint(white),
-                            modifier = Modifier
-                                .rotate(180f)
-                                .padding(vertical = 8.dp)
-                        )
-                    }
-                    )
+        }
+    }
+
+    Box {
+        if (filterOpened) {
+            filter.Content()
+        }
+        if (searchOpened) {
+            search.Content()
+        }
+        if (Template.CatalogHeader != null) {
+            Template.CatalogHeader?.let {
+                it(::openFilter, ::openSearch, goToFavorite, goToBack, ::getActiveFilterCount)
+            }
+        } else {
+            Column(Modifier.background(color = primary)) {
+                if (showFullHeader) {
+                    HeaderTitle()
                 }
-                Row {
-
-                    Clickable(onClick = { openSearch() }, children = {
-                        Surface(
-                            shape = CircleShape,
-                            elevation = 8.dp,
-                            modifier = Modifier.padding(horizontal = 10.dp)
-                        ) {
-                            Box(
-                                Modifier
-                                    .background(white)
-                                    .padding(8.dp)
-                            )
-                            {
-//                                Image(
-//                                    painter = painterResource(search),
-//                                    contentDescription = null,
-//                                    colorFilter = ColorFilter.tint(primary),
-//                                )
-                            }
-                        }
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(12.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = if (showFullHeader) Arrangement.Start else Arrangement.SpaceBetween
+                ) {
+                    if (!showFullHeader) {
+                        BackButton()
                     }
-                    )
-
-                    Clickable(onClick = { openFilter() }, children = {
-                        Box {
-                            Surface(
-                                shape = CircleShape,
-                                elevation = 8.dp,
-                                modifier = Modifier.padding(horizontal = 10.dp)
-                            ) {
-                                Row(
-                                    Modifier
-                                        .background(white)
-                                        .padding(8.dp)
-                                )
-                                {
-//                                    Image(
-//                                        painter = painterResource(filter),
-//                                        contentDescription = null,
-//                                        colorFilter = ColorFilter.tint(primary),
-//                                    )
-
-                                }
-                            }
-                            if (getActiveFilterCount() != 0) {
-                                Box(
-                                    modifier = Modifier
-                                        .size(20.dp)
-                                        .clip(CircleShape)
-                                        .background(Color.Red)
-                                        .align(Alignment.TopEnd)
-                                ) {
-                                    Text(
-                                        text = getActiveFilterCount().toString(),
-                                        color = white,
-                                        modifier = Modifier.align(Alignment.Center)
-
-                                    )
-                                }
-                            }
-                        }
-                    })
-
-                    if (!isFavorit) {
-                        if (showFullHeader) {
-                            Box(modifier = Modifier
-                                .padding(horizontal = 10.dp)
-                                .border(
-                                    border = BorderStroke(1.dp, white),
-                                    shape = RoundedCornerShape(50)
-                                )
-                                .clickable { goToFavorite() }
-                            ) {
-                                Row(
-                                    Modifier
-                                        .padding(horizontal = 16.dp, vertical = 8.dp)
-                                        .background(primary),
-                                    verticalAlignment = Alignment.CenterVertically
-                                ) {
-                                    Image(
-                                        painter = painterResource(favorite),
-                                        contentDescription = null,
-                                        colorFilter = ColorFilter.tint(white),
-                                        modifier = Modifier.padding(end = 8.dp)
-                                    )
-                                    Text(
-                                        text = favoriteButtonText,
-                                        color = white
-                                    )
-                                }
-                            }
-                        } else {
-                            Clickable(onClick = { goToFavorite() }, children = {
-                                Surface(
-                                    shape = CircleShape,
-                                    elevation = 8.dp,
-                                    modifier = Modifier.padding(horizontal = 10.dp)
-                                ) {
-                                    Box(
-                                        Modifier
-                                            .background(white)
-                                            .padding(8.dp)
-                                    )
-                                    {
-                                        Image(
-                                            painter = painterResource(favorite),
-                                            contentDescription = null,
-                                            colorFilter = ColorFilter.tint(primary),
-                                        )
-                                    }
-                                }
-                            }
-                            )
-                        }
-
+                    Row {
+                        SearchButton()
+                        FilterButton()
+                        FavoriteButton()
                     }
                 }
             }
