f5141543ff96b1f78b953d7d55ed80b8672733ee
tibo <thibault.corbrion@miam.tech>
2022-10-13 11:48:34 +0200

    default number of result and init from criteria

--- miamCore/src/commonMain/kotlin/com/miam/kmmMiamCore/component/recipeCarousel/RecipeCarouselContract.kt
+++ miamCore/src/commonMain/kotlin/com/miam/kmmMiamCore/component/recipeCarousel/RecipeCarouselContract.kt
@@ -5,12 +5,14 @@ import com.miam.kmmMiamCore.base.mvi.UiEffect
 import com.miam.kmmMiamCore.base.mvi.UiEvent
 import com.miam.kmmMiamCore.base.mvi.UiState
 import com.miam.kmmMiamCore.miam_core.model.Recipe
+import com.miam.kmmMiamCore.miam_core.model.SuggestionsCriteria
 
 
 interface RecipeCarouselContract {
 
     sealed class Event : UiEvent {
-        data class GetRecipeSuggestions(val productId: String, val numberOfResult: Int?) : Event()
+        data class GetRecipeSuggestionsId(val productId: String, val numberOfResult: Int = 4) : Event()
+        data class GetRecipeSuggestionsFromCriteria(val criteria: SuggestionsCriteria, val numberOfResult: Int = 4) : Event()
     }
 
     data class State(val suggestions: BasicUiState<List<Recipe>>) : UiState
