81f6f92ebd5d2e60c003a9467d36328187056430
Vincent Kergonna <kergonna@k-interactive.fr>
2022-11-03 16:38:21 +0100

    Add MyMealButtonView
    Add success view, empty view and view model

---
+++ MiamIOSFramework/MiamIOSFramework/ui/myMealButton/MyMealButtonEmptyView.swift
@@ -0,0 +1,16 @@
+//
+//  MyMealButtonEmptyView.swift
+//  MiamIOSFramework
+//
+//  Created by Vincent Kergonna on 03/11/2022.
+//  Copyright © 2022 Miam. All rights reserved.
+//
+
+import SwiftUI
+
+@available(iOS 14, *)
+struct MyMealButtonEmptyView: View {
+    var body: some View {
+        EmptyView()
+    }
+}
---
+++ MiamIOSFramework/MiamIOSFramework/ui/myMealButton/MyMealButtonSuccessView.swift
@@ -0,0 +1,35 @@
+//
+//  MyMealButtonSuccessView.swift
+//  MiamIOSFramework
+//
+//  Created by Vincent Kergonna on 03/11/2022.
+//  Copyright © 2022 Miam. All rights reserved.
+//
+
+import SwiftUI
+
+@available(iOS 14, *)
+struct MyMealButtonSuccessView: View {
+    let mealsCount: Int
+    let onButtonTapped: () -> Void
+    var body: some View {
+        Button {
+            onButtonTapped()
+        } label: {
+            VStack {
+                Image.miamImage(icon: .greyChevronDown)
+                    .rotationEffect(Angle(degrees: 180.0))
+                
+                Text("\(mealsCount) repas ajouté").bold()
+                    
+            }.padding(EdgeInsets(top: 16.0, leading: 20.0, bottom: 8.0, trailing: 20.0))
+             .frame(maxWidth: .infinity)
+             .background(Color.miamColor(.primaryDark))
+             .foregroundColor(Color.miamColor(.white))
+              // Little trick to have corner radius only on top corners
+             .padding(.bottom, 20.0)
+             .cornerRadius(20.0)
+             .padding(.bottom, -20.0)
+        }
+    }
+}
