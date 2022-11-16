0084d4e534f1d12e77893484ba456dfa7c1b0d57
tibo <thibault.corbrion@miam.tech>
2022-11-16 14:18:48 +0100

    fix coding style

--- androidSDK/src/main/java/com/miam/kmm_miam_sdk/android/ui/components/recipeCard/RecipeCardText.kt
+++ androidSDK/src/main/java/com/miam/kmm_miam_sdk/android/ui/components/recipeCard/RecipeCardText.kt
@@ -4,5 +4,4 @@ package com.miam.kmm_miam_sdk.android.ui.components.recipeCard
 object RecipeCardText {
     var isInCart = "Voir le détail"
     var addToCart = "Ajouter les ingrédients"
-
 }
--- changelog.md
+++ changelog.md
@@ -1,5 +1,6 @@
 ## 3.1.0
 - [FIX][AND] Fix Recipe card CTA text is now customizable
+
 ## 3.0.1
 - [FIX][AND] Fix loader on prefrences screen
 - [FIX][AND] Disable guests pref
