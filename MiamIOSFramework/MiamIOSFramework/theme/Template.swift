2d31234c1756bd263e3c4dee6cc5646659ff30e2
Vincent Kergonna <kergonna@k-interactive.fr>
2022-11-23 15:46:24 +0100

    Add missing @escaping keyword on templates’ closures

--- MiamIOSFramework/MiamIOSFramework/theme/Template.swift
+++ MiamIOSFramework/MiamIOSFramework/theme/Template.swift
@@ -340,7 +340,7 @@ public class Template {
      */
     public var likeButtonTemplate: ((
         Bool,
-        () -> Void
+        @escaping () -> Void
     ) -> AnyView)? = nil
    
     /**
@@ -386,7 +386,7 @@ public class Template {
      */
     public var myMealButtonSuccessViewTemplate: ((
         Int,
-        () -> Void
+        @escaping () -> Void
     ) -> AnyView)? = nil
     
     /**
@@ -413,11 +413,11 @@ public class Template {
         [CheckableTag],
         [CheckableTag],
         Int,
-        (Int) -> Void,
-        (CheckableTag) -> Void,
-        () -> Void,
-        () -> Void,
-        () -> Void
+        @escaping (Int) -> Void,
+        @escaping (CheckableTag) -> Void,
+        @escaping () -> Void,
+        @escaping () -> Void,
+        @escaping () -> Void
     ) -> AnyView)? = nil
     
     /**
@@ -426,7 +426,7 @@ public class Template {
      */
     public var preferenceListItemViewTemplate: ((
         CheckableTag,
-        (CheckableTag) -> Void
+        @escaping (CheckableTag) -> Void
     ) -> AnyView)? = nil
     
     /**
@@ -439,7 +439,7 @@ public class Template {
         String,
         String,
         [CheckableTag],
-        (CheckableTag) -> Void
+        @escaping (CheckableTag) -> Void
     ) -> AnyView)? = nil
    
     /**
@@ -455,8 +455,8 @@ public class Template {
         String,
         [CheckableTag],
         GeometryProxy,
-        (CheckableTag) -> Void,
-        () -> Void
+        @escaping (CheckableTag) -> Void,
+        @escaping () -> Void
     ) -> AnyView)? = nil
     
    
@@ -464,7 +464,7 @@ public class Template {
      onTapped: () -> Void
      */
     public var addTagViewTemplate: ((
-        () -> Void
+        @escaping () -> Void
     ) -> AnyView)? = nil
     
     /**
@@ -473,7 +473,7 @@ public class Template {
      */
     public var preferencesTagViewTemplate: ((
         CheckableTag,
-        (CheckableTag) -> Void
+        @escaping (CheckableTag) -> Void
     ) -> AnyView)? = nil
     
     
@@ -483,8 +483,8 @@ public class Template {
      numberOfRecipesFound: Int
      */
     public var preferencesFooterView: ((
-        () -> Void,
-        () -> Void,
+        @escaping () -> Void,
+        @escaping () -> Void,
         Int
     ) -> AnyView)? = nil
     
@@ -494,6 +494,6 @@ public class Template {
      */
     public var preferencesSearchViewTemplate: ((
         PreferencesSearchVM,
-        () -> Void
+        @escaping () -> Void
     ) -> AnyView)? = nil
 }
