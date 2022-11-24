a3a60f6ef12bcfa868831c2a3c7c03c9a2013125
Vincent Kergonna <kergonna@k-interactive.fr>
2022-11-24 14:40:23 +0100

    Make counterView variable public

--- MiamIOSFramework/MiamIOSFramework/ui/RecipeDetails/RecipeDetailsIngredientsView.swift
+++ MiamIOSFramework/MiamIOSFramework/ui/RecipeDetails/RecipeDetailsIngredientsView.swift
@@ -16,7 +16,7 @@ struct RecipeDetailsIngredientsView: View {
     let increaseGuestsAction: () -> Void
     let decreaseGuestsAction: () -> Void
     
-    var counterView: CounterView
+    public var counterView: CounterView
     init(ingredients: [Ingredient], recipeGuests: Int, currentGuests: Int, increaseGuestsAction: @escaping () -> Void, decreaseGuestsAction: @escaping () -> Void) {
         self.ingredients = ingredients
         self.recipeGuests = recipeGuests
