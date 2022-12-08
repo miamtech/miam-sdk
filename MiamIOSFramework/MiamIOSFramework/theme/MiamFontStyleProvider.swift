b9172a3b85bcb6cf643f04253ab5dbd800920599
Vincent Kergonna <kergonna@k-interactive.fr>
2022-12-08 17:54:32 +0100

    Add font styles

--- MiamIOSFramework/MiamIOSFramework/theme/MiamFontStyleProvider.swift
+++ MiamIOSFramework/MiamIOSFramework/theme/MiamFontStyleProvider.swift
@@ -17,7 +17,31 @@ public struct ButtonFontStyle: MiamFontStyle {
 
 @available(iOS 14, *)
 public struct TitleFontStyle: MiamFontStyle {
-    var font: Font = Font.system(size: 15, weight: .bold, design: .default)
+    var font: Font = Font.system(size: 20, weight: .semibold, design: .default)
+    var color: Color? = nil
+}
+
+@available(iOS 14, *)
+public struct TitleMediumFontStyle: MiamFontStyle {
+    var font: Font = Font.system(size: 16, weight: .semibold, design: .default)
+    var color: Color? = nil
+}
+
+@available(iOS 14, *)
+public struct TitleSmallFontStyle: MiamFontStyle {
+    var font: Font = Font.system(size: 14, weight: .semibold, design: .default)
+    var color: Color? = nil
+}
+
+@available(iOS 14, *)
+public struct TitleExtraSmallFontStyle: MiamFontStyle {
+    var font: Font = Font.system(size: 13, weight: .semibold, design: .default)
+    var color: Color? = nil
+}
+
+@available(iOS 14, *)
+public struct TitleExtraSmallMediumFontStyle: MiamFontStyle {
+    var font: Font = Font.system(size: 13, weight: .medium, design: .default)
     var color: Color? = nil
 }
 
@@ -28,18 +52,84 @@ public struct SubtitleFontStyle: MiamFontStyle {
 }
 
 @available(iOS 14, *)
+public struct BodyBigFontStyle: MiamFontStyle {
+    var font: Font = Font.system(size: 16, design: .default)
+    var color: Color? = nil
+}
+
+@available(iOS 14, *)
+public struct BodyBigBoldFontStyle: MiamFontStyle {
+    var font: Font = Font.system(size: 16, weight: .bold, design: .default)
+    var color: Color? = nil
+}
+
+@available(iOS 14, *)
+public struct BodyBigLightFontStyle: MiamFontStyle {
+    var font: Font = Font.system(size: 16, weight: .light, design: .default)
+    var color: Color? = nil
+}
+
+@available(iOS 14, *)
 public struct BodyFontStyle: MiamFontStyle {
+    var font: Font = Font.system(size: 15, design: .default)
+    var color: Color? = nil
+}
+
+@available(iOS 14, *)
+public struct BodyBoldFontStyle: MiamFontStyle {
     var font: Font = Font.system(size: 15, weight: .bold, design: .default)
     var color: Color? = nil
 }
- 
- 
+
+@available(iOS 14, *)
+public struct BodyMediumFontStyle: MiamFontStyle {
+    var font: Font = Font.system(size: 14, design: .default)
+    var color: Color? = nil
+}
+
+@available(iOS 14, *)
+public struct BodyMediumBoldFontStyle: MiamFontStyle {
+    var font: Font = Font.system(size: 14, weight: .bold, design: .default)
+    var color: Color? = nil
+}
+
+@available(iOS 14, *)
+public struct BodySmallFontStyle: MiamFontStyle {
+    var font: Font = Font.system(size: 12, design: .default)
+    var color: Color? = nil
+}
+
+@available(iOS 14, *)
+public struct BodySmallBoldFontStyle: MiamFontStyle {
+    var font: Font = Font.system(size: 12, weight: .bold, design: .default)
+    var color: Color? = nil
+}
+
+@available(iOS 14, *)
+public struct BodyExtraSmallFontStyle: MiamFontStyle {
+    var font: Font = Font.system(size: 11, design: .default)
+    var color: Color? = nil
+}
+
 @available(iOS 14, *)
 public struct MiamFontStyleProvider {
     static let sharedInstance = MiamFontStyleProvider()
     
     public var titleStyle = TitleFontStyle()
+    public var titleMediumStyle = TitleMediumFontStyle()
+    public var titleSmallStyle = TitleSmallFontStyle()
+    public var titleExtraSmallStyle = TitleExtraSmallFontStyle()
+    public var titleExtraSmallMediumStyle = TitleExtraSmallMediumFontStyle()
     public var subtitleStyle = SubtitleFontStyle()
     public var buttonStyle = ButtonFontStyle()
     public var bodyStyle = BodyFontStyle()
+    public var bodyBigStyle = BodyBigFontStyle()
+    public var bodyBigLightStyle = BodyBigLightFontStyle()
+    public var bodyBigBoldStyle = BodyBigBoldFontStyle()
+    public var bodyBoldStyle = BodyBoldFontStyle()
+    public var bodyMediumStyle = BodyMediumFontStyle()
+    public var bodyMediumBoldStyle = BodyMediumBoldFontStyle()
+    public var bodySmallStyle = BodySmallFontStyle()
+    public var bodySmallBoldStyle = BodySmallBoldFontStyle()
+    public var bodyExtraSmallStyle = BodyExtraSmallFontStyle()
 }
