2546f0786b42fea6567954e6871338065b5adce0
Vincent Kergonna <kergonna@k-interactive.fr>
2022-12-12 14:03:59 +0100

    Add PreferenceTagView and AddTagView public initializers

--- MiamIOSFramework/MiamIOSFramework/ui/preferences/PreferencesTagsListView.swift
+++ MiamIOSFramework/MiamIOSFramework/ui/preferences/PreferencesTagsListView.swift
@@ -98,6 +98,10 @@ public struct PreferencesTagsListView: View {
 @available(iOS 14, *)
 public struct AddTagView: View {
     public let onTapped: () -> Void
+   
+    public init(onTapped: @escaping () -> Void) {
+        self.onTapped = onTapped
+    }
     
     public var body: some View {
         if let template = Template.sharedInstance.addTagViewTemplate {
@@ -150,6 +154,10 @@ public struct PreferenceTagView: View {
         }
     }
     
+    public init(tag: CheckableTag, onToggleTag: @escaping (CheckableTag) -> Void) {
+        self.tag = tag
+        self.onToggleTag = onToggleTag
+    }
     
     public var body: some View {
         if let template = Template.sharedInstance.preferencesTagViewTemplate {
