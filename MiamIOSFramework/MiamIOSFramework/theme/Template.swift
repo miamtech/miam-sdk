b5719c6690e8516d5683804dc00ebe4338824c64
thibault.corbrion <thibault.corbrion@miam.tech>
2023-01-30 10:24:58 +0100

    Add generic error template

--- MiamIOSFramework/MiamIOSFramework/theme/Template.swift
+++ MiamIOSFramework/MiamIOSFramework/theme/Template.swift
@@ -580,4 +580,10 @@ public class Template {
         RecipeCardVM,
         @escaping () -> Void
     ) -> AnyView)? = nil
+    
+    /**
+     Error Template
+     */
+    public var ErrorTemplate: ((
+    ) -> AnyView)? = nil
 }
