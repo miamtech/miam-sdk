106965863e52c65ecf87be2351963b634aa31edb
Vincent Kergonna <kergonna@k-interactive.fr>
2023-03-14 11:52:46 +0100

    Update iOS like button view model
    Publish the current state, fix deprecated init warning on Bool and remove force unwrap on state.

--- MiamIOSFramework/MiamIOSFramework/ViewModels/LikeButtonVM.swift
+++ MiamIOSFramework/MiamIOSFramework/ViewModels/LikeButtonVM.swift
@@ -13,14 +13,15 @@ import miamCore
 @available(iOS 14, *)
 public class LikeButtonVM: LikeButtonViewModel, ObservableObject {
     @Published public var isLiked: Bool = false
+    @Published public var state: RecipeLikeContractState?
     
     public override init() {
         super.init()
         collect(flow: uiState) { data in
-            let state = data as! RecipeLikeContractState
-            switch state.isLiked {
+            self.state = data as? RecipeLikeContractState
+            switch self.state?.isLiked {
                 case let success as BasicUiStateSuccess<KotlinBoolean>:
-                self.isLiked = Bool(success.data!)
+                self.isLiked = Bool(truncating: success.data!)
                 default:
                     break
             }
