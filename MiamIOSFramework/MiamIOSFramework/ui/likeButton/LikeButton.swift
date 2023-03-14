c73f9a43d6e831b508a8c6c31b26ca63f049e3a3
Vincent Kergonna <kergonna@k-interactive.fr>
2023-03-14 12:13:47 +0100

    Disable like button while updating like state

--- MiamIOSFramework/MiamIOSFramework/ui/likeButton/LikeButton.swift
+++ MiamIOSFramework/MiamIOSFramework/ui/likeButton/LikeButton.swift
@@ -20,35 +20,48 @@ public struct LikeButton: View {
 
     public var body: some View {
         HStack {
-            if let template = Template.sharedInstance.likeButtonTemplate {  
+            if let template = Template.sharedInstance.likeButtonTemplate {
                 template(self.viewModel.isLiked, { self.viewModel.toggleLike() })
             } else {
-                ZStack(){
-                    Circle().fill(Color.miamColor(.white))
-                        .frame(width: 40, height: 40)
-                    if(self.viewModel.isLiked){
-                        Image.miamImage(icon: .likeFilled)
-                            .renderingMode(.template)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 26.0, height: 26.0, alignment: .center)
-                            .foregroundColor(Color.miamColor(.primaryText))
-                            .padding(.top,3)
-                    } else {
-                        Image.miamImage(icon: .like)
-                            .renderingMode(.template)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 26.0, height: 26.0, alignment: .center)
-                            .foregroundColor(Color.miamColor(.primaryText))
-                            .padding(.top,3)
-                    }
-                }.onTapGesture {
-                    self.viewModel.toggleLike()
+                if let state = viewModel.state {
+                    ManagementResourceState(resourceState: state.isLiked,
+                                            successView: LikeButtonSuccessView(isLiked: viewModel.isLiked)
+                                                        .onTapGesture {
+                                                            self.viewModel.toggleLike()
+                                                        },
+                                            loadingView: LikeButtonSuccessView(isLiked: viewModel.isLiked),
+                                            emptyView: EmptyView())
                 }
+
             }
         }
-        .onAppear(perform: self.viewModel.listenRecipeLikeChanges)
-        .onDisappear(perform: self.viewModel.stopListenRecipeLikeChanges)
+    }
+}
+
+@available(iOS 14, *)
+private struct LikeButtonSuccessView: View {
+    let isLiked: Bool
+    public var body: some View {
+        ZStack(){
+            Circle().fill(Color.miamColor(.white))
+                .frame(width: 40, height: 40)
+            if(isLiked){
+                Image.miamImage(icon: .likeFilled)
+                    .renderingMode(.template)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 26.0, height: 26.0, alignment: .center)
+                    .foregroundColor(Color.miamColor(.primaryText))
+                    .padding(.top,3)
+            } else {
+                Image.miamImage(icon: .like)
+                    .renderingMode(.template)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 26.0, height: 26.0, alignment: .center)
+                    .foregroundColor(Color.miamColor(.primaryText))
+                    .padding(.top,3)
+            }
+        }
     }
 }
